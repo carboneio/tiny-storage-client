636bab2a96b592538c100be2e57dfd98e3fd9d27
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2023-03-08 18:59:58 +0400

    Add tests

--- tests/s3.test.js
+++ tests/s3.test.js
@@ -33,6 +33,12 @@ describe('S3 SDK', function () {
 
   });
 
+  describe('headBucket', function() {
+
+
+
+  });
+
   describe('listFiles', function() {
 
     describe("SINGLE STORAGE", function () {
@@ -46,6 +52,105 @@ describe('S3 SDK', function () {
 
   });
 
+  describe('downloadFile', function() {
 
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('uploadFile', function() {
+
+    describe("SINGLE STORAGE", function () {
+
+      it("should upload a file provided as buffer", function() {
+
+      })
+
+      it("should upload a file provided as local path", function() {
+
+      })
+
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('deleteFile', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('deleteFiles', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('deleteFiles', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('getFileMetadata', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+
+  describe('setFileMetadata', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
 
 });
