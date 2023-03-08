018812ce64ca4d1fb0eb0dcee71be701cedb2ede
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2023-03-08 18:46:44 +0400

    Update readme

--- tests/s3.test.js
+++ tests/s3.test.js
@@ -3,5 +3,49 @@ const assert = require('assert')
 
 describe('S3 SDK', function () {
 
+  describe('setConfig/getConfig/setTimeout', function () {
+
+  });
+
+  describe('request - CALLBACK', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('request - STREAM', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+  describe('listFiles', function() {
+
+    describe("SINGLE STORAGE", function () {
+    });
+
+    describe("MULTIPLE STORAGE", function () {
+    });
+
+    describe("PARALLEL REQUESTS", function () {
+    })
+
+  });
+
+
 
 });
